379fa9d00014e14ab60aa0a52ef9d6477610ae30
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-11 12:02:52 +0200

    Create MainActivity.java

---
+++ test-beta-main/config/android/app/src/main/java/org/example/testapp/MainActivity.java
@@ -0,0 +1,7 @@
+package org.example.testapp;
+
+import org.qtproject.qt.android.bindings.QtActivity;
+
+public class MainActivity extends QtActivity {
+    // Qt utilise cette activité pour lancer l'application Android
+}

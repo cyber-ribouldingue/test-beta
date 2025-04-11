0c7eeaea97e360449c4f97cd7ee00f0cf186e333
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-11 08:04:00 +0200

    Create dummy_codeql.cpp

---
+++ src/dummy_codeql.cpp
@@ -0,0 +1,10 @@
+// Fichier utilisé uniquement pour que CodeQL détecte du code C++ natif
+#include <string>
+
+std::string getVersion() {
+    return "1.0.0";
+}
+
+int add(int a, int b) {
+    return a + b;
+}

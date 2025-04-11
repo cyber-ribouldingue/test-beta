814a1ec33eee29fad8a6837ed1d51b4a3164f01f
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-11 09:40:46 +0200

    Create MainActivity.java

---
+++ config/android/app/src/main/java/org/example/testapp/MainActivity.java
@@ -0,0 +1,7 @@
+package org.example.testapp;
+
+import org.qtproject.qt.android.bindings.QtActivity;
+
+public class MainActivity extends QtActivity {
+    // Qt gère tout. Ce fichier est requis pour que l'application Android démarre.
+}

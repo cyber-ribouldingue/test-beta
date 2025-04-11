02f600048b45be507f7b8c793907f20dccb9db83
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-11 10:12:05 +0200

    Update hello_ios.cpp

--- src/hello_ios.cpp
+++ src/hello_ios.cpp
@@ -1,4 +1,4 @@
-// Fichier de test pour débloquer la compilation iOS
+// Fichier test pour débloquer la compilation iOS
 int dummy_ios_function()
 {
     return 42; // valeur arbitraire pour valider la compilation

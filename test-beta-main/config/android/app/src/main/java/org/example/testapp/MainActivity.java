e659ab75ef5ae12462e956414e3f11efd61f7e28
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-11 12:03:46 +0200

    Delete test-beta-main/config/android/app/src/main/java/org/example/testapp directory

--- test-beta-main/config/android/app/src/main/java/org/example/testapp/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package org.example.testapp;
-
-import org.qtproject.qt.android.bindings.QtActivity;
-
-public class MainActivity extends QtActivity {
-    // Qt utilise cette activité pour lancer l'application Android
-}

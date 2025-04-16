d07a9797d173d48f640a2c562cad42212148090c
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-16 08:32:24 +0200

    Update cpp.cmake

--- config/cpp.cmake
+++ config/cpp.cmake
@@ -1,4 +1,6 @@
-
+# config/cpp.cmake
+set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-set(CMAKE_CXX_STANDARD 17)
+# Options de build sécurisées
+add_compile_options(-Wall -Wextra -Wpedantic)

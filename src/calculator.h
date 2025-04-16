ba08d1e6b3f5407c325c8d43404c7118b18b6742
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-16 12:05:49 +0200

    Create calculator.h

---
+++ src/calculator.h
@@ -0,0 +1,11 @@
+// src/calculator.h
+#pragma once
+#include <stdexcept>
+
+class Calculator {
+public:
+    int add(int a, int b);
+    int subtract(int a, int b);
+    int multiply(int a, int b);
+    int divide(int a, int b);
+};

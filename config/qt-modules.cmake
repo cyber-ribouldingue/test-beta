1e020acff88e99df1fc3f5cde74ef2f7bd1d1127
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-16 08:33:59 +0200

    Create qt-modules.cmake

---
+++ config/qt-modules.cmake
@@ -0,0 +1,12 @@
+# config/qt-modules.cmake
+
+set(QT_REQUIRED_MODULES
+    Qt6::Core
+    Qt6::Gui
+    Qt6::Widgets
+    Qt6::Qml
+    Qt6::Quick
+    Qt6::QuickControls2
+    Qt6::QuickTest
+    Qt6::Test
+)

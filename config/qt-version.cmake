e5754122d92faf4539faa49c510fdaad3f055ed5
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-16 08:33:07 +0200

    Update and rename qt.cmake to qt-version.cmake

---
+++ config/qt-version.cmake
@@ -0,0 +1,4 @@
+# config/qt-version.cmake
+set(QT_VERSION_MAJOR 6)
+set(QT_VERSION_MINOR 8)
+set(QT_VERSION_PATCH 3)
--- config/qt.cmake
+++
@@ -1,4 +0,0 @@
-
-find_package(Qt6 REQUIRED COMPONENTS Quick)
-
-qt_standard_project_setup(REQUIRES 6.8)

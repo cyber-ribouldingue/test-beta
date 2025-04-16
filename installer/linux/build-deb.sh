561320cf2f17b8bf8bf68286f83baca08055aa35
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-16 12:37:13 +0200

    Update build-deb.sh

--- installer/linux/build-deb.sh
+++ installer/linux/build-deb.sh
@@ -4,17 +4,25 @@ APP_NAME="TestApp"
 VERSION="1.0.0"
 ARCH="amd64"
 BUILD_DIR="build"
-INSTALL_DIR="installer/linux"
-PACKAGE_NAME="testapp_${VERSION}_${ARCH}.deb"
+PKG_DIR="installer/linux"
+DEB_NAME="testapp_${VERSION}_${ARCH}.deb"
 
 # 1. Nettoyage
-rm -rf "${INSTALL_DIR}/usr"
-mkdir -p "${INSTALL_DIR}/usr/local/bin"
+rm -rf "${PKG_DIR}/usr"
+mkdir -p "${PKG_DIR}/usr/local/bin"
+mkdir -p "${PKG_DIR}/usr/share/applications"
+mkdir -p "${PKG_DIR}/usr/share/icons/hicolor/64x64/apps"
 
-# 2. Copie de l'exécutable compilé
-cp "${BUILD_DIR}/${APP_NAME}" "${INSTALL_DIR}/usr/local/bin/"
+# 2. Copier l'exécutable
+cp "${BUILD_DIR}/${APP_NAME}" "${PKG_DIR}/usr/local/bin/"
 
-# 3. Génération du .deb
-dpkg-deb --build "${INSTALL_DIR}" "${PACKAGE_NAME}"
+# 3. Copier le fichier .desktop
+cp "${PKG_DIR}/testapp.desktop" "${PKG_DIR}/usr/share/applications/"
 
-echo "✅ Paquet généré : ${PACKAGE_NAME}"
+# 4. Copier l'icône
+cp "${PKG_DIR}/testapp.png" "${PKG_DIR}/usr/share/icons/hicolor/64x64/apps/"
+
+# 5. Créer le paquet .deb
+dpkg-deb --build "${PKG_DIR}" "${DEB_NAME}"
+
+echo "✅ Paquet généré : ${DEB_NAME}"

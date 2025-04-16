057cc754a747b1e72131c591a0d7a715ae753807
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-16 12:06:07 +0200

    Create calculator.cpp

---
+++ src/calculator.cpp
@@ -0,0 +1,16 @@
+// src/calculator.cpp
+#include "calculator.h"
+
+int Calculator::add(int a, int b) {
+    return a + b;
+}
+int Calculator::subtract(int a, int b) {
+    return a - b;
+}
+int Calculator::multiply(int a, int b) {
+    return a * b;
+}
+int Calculator::divide(int a, int b) {
+    if (b == 0) throw std::runtime_error("Division by zero");
+    return a / b;
+}

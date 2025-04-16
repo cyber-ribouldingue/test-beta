aa912515b004b89ad7b9433de0f222e13dc097df
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-16 09:10:39 +0200

    Delete config/android/app/src/main/java/org/example/testapp/MainActivity.java

--- config/android/app/src/main/java/org/example/testapp/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package org.example.testapp;
-
-import org.qtproject.qt.android.bindings.QtActivity;
-
-public class MainActivity extends QtActivity {
-    // Qt gère tout. Ce fichier est requis pour que l'application Android démarre.
-}

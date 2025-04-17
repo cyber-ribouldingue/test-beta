cda00e1f9dfbdfb6eb4047f4127196ed83700959
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-17 17:32:33 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,14 @@
-// src/main.cpp
-
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QQuickStyle>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-    QQuickStyle::setStyle("Material");
 
     QQmlApplicationEngine engine;
-
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-                     &app, []() { QCoreApplication::exit(-1); },
-                     Qt::QueuedConnection);
-
+    QObject::connect(
+        &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
+        []() { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
     engine.loadFromModule("TestApp", "Main");
 
     return app.exec();

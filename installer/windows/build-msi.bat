3ac1318cdc7ab5411a412339aa906af3334b922d
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-17 07:45:53 +0200

    Update build-msi.bat

--- installer/windows/build-msi.bat
+++ installer/windows/build-msi.bat
@@ -1,10 +1,24 @@
 @echo off
+setlocal
+
 echo [*] Compilation de TestApp.exe
-cmake -B build -DCMAKE_BUILD_TYPE=Release
+
+:: Configure la variable Qt
+set "QT_PATH=%USERPROFILE%\Qt\6.8.3\msvc2019_64"
+
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QT_PATH%
 cmake --build build --config Release
+
+if not exist build\Release\TestApp.exe (
+    echo [!] Erreur : build\Release\TestApp.exe introuvable.
+    exit /b 1
+)
 
 echo [*] Création du fichier MSI avec WiX
 candle installer\windows\installer.wxs
-light installer.wixobj -o TestAppInstaller.msi
+if errorlevel 1 exit /b 1
 
-echo [✔] Fichier généré : TestAppInstaller.msi
+light installer.wixobj -o TestAppInstaller.msi
+if errorlevel 1 exit /b 1
+
+echo [✔] Fichier MSI généré : TestAppInstaller.msi
